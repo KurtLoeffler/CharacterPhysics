cc4e95a92ed4a7006e2e16bccbd85333047e1502
kurtloeffler <kurtloeffler@gmail.com>
2015-09-09 14:03:53 -0700

    no message

---
+++ IMovingPlatform.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CharacterPhysics
+{
+	public interface IMovingPlatform
+	{
+		GameObject gameObject { get; }
+		bool sticky { get; set; }
+		Vector3 GetVelocityAtPoint(Vector3 point);
+    }
+}
+
